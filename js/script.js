d4aa249731b666fcf2983ef4d2efcf3dc8bbf106
danielld75 <danielld@poczta.fm>
2017-11-22 21:28:33 +0100

    Add commit with comments

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//alert("Hello my friends!");
+console.log("Hello my friends!");
+/*
+lines comment
+multilines comment
+:)
+ */
+alert("My comments are super :)");
+//console.log("My comments are super :)");
